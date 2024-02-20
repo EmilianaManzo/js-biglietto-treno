3ca36db5e28cc6c4666d1cc768bfc8a6de04b53a
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-20 15:53:38 +0100

    [add] discount

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,24 @@
-const km = prompt('Quanti chilometri percorrerai?');
+const km = parseFloat (prompt('Quanti chilometri percorrerai?'));
 
-const età = prompt('Quanti anni hai?');
+const età = parseInt (prompt('Quanti anni hai?'));
 
-const priceKm = '0.21' 
+const priceKm = 0.21; 
 
-const price = km * priceKm
-
+const price = km * priceKm;
 
 console.log(km, età, price);
+
+const youngerDiscount = price - (price * 20 / 100);
+
+const olderDiscount = price - (price * 40 / 100);
+
+console.log(youngerDiscount, olderDiscount);
+
+
+
+// if (età < 18) {
+//   younger
+// }else if (età >= 65) {
+//   older 
+// } 
+
