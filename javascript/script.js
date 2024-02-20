4bec56d7082ae4ebf035ea8d1bfdc37361571205
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-20 16:22:42 +0100

    [add] 5-6-7

--- javascript/script.js
+++ javascript/script.js
@@ -8,6 +8,7 @@ const price = km * priceKm;
 
 console.log(km, età, price);
 
+
 const youngerDiscount = price - (price * 20 / 100);
 
 const olderDiscount = price - (price * 40 / 100);
@@ -16,9 +17,10 @@ console.log(youngerDiscount, olderDiscount);
 
 
 
-// if (età < 18) {
-//   younger
-// }else if (età >= 65) {
-//   older 
-// } 
+if (età <= 17) {
+  youngerDiscount
+}else if (età >= 65) {
+  olderDiscount
+}
 
+document.getElementById('output').innerHTML += price.toFixed(2) ;
